80e0f678f66853eeedcbca06e57a81dd10877c66
Konrad570 <Konradtoesch@icloud.com>
2021-08-18 19:11:42 +0200

    UPDATE

--- src/commands/ℹ️ Information/botinfo.js
+++ src/commands/ℹ️ Information/botinfo.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const User = require("../../database/schemas/User");
 const moment = require("moment");
 require("moment-duration-format");
+const { stripIndent } = require('common-tags');
 
 module.exports = {
   name: "botinfo",
@@ -19,21 +20,26 @@ module.exports = {
   run: async (client, message, args, user, guild) => {
     const users = await User.find();
 
+
+    const infos = stripIndent`
+    Bot Information
+    • Ping     : ${client.ws.ping}ms
+    • Uptime   : ${moment.duration(client.uptime).format("H [hours and] m [minutes]")}
+    • Servers  : ${client.guilds.cache.size}
+    • Users    : ${client.users.cache.size}
+    • Channels : ${client.channels.cache.size}\n
+    Database Information: 
+    - Users    : ${users.length} users
+    - Total XP : ${users.length > 1 ? users.reduce((a, b) => {return a.xp || 0 + b.xp || 0;}): users.xp || 0} XP\`
+    `;
+    	
     const embed = new Discord.MessageEmbed()
       .setTitle("Cody's Information")
       .setColor(message.guild.me.displayHexColor)
       .setTimestamp()
       .setFooter("cody-bot.xyz",  message.author.displayAvatarURL({ dynamic: true }))
-      .setDescription(
-        `**Ping:** \`${client.ws.ping}ms\`\n
-        **Uptime:** \`${moment.duration(client.uptime).format("H [hours and] m [minutes]")}\`\n
-        **Servers:** \`${client.guilds.cache.size}\`\n
-        **Users:** \`${client.users.cache.size}\`\n
-        **Channels:** \`${client.channels.cache.size}\`\n
-        **CPU / RAM:** \`AMD Ryzen 5 5600X 6-Core | 3.85GB / 16GB\`\n\n
-        **♨️ __Database Information:__ ♨️**\n
-        **Users:** \`${users.length} users\`\n
-        **Total XP:** \`${users.length > 1 ? users.reduce((a, b) => {return a.xp || 0 + b.xp || 0;}): users.xp || 0} XP\``);
+      .setDescription(`\`\`\`diff\n${infos}\`\`\``)
+        
 
     return message.channel.send({ embed: embed });
   },
