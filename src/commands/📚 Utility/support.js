1803f814aebd06156eca40d573ccc8a52d9af381
Konrad570 <Konradtoesch@icloud.com>
2021-08-25 17:06:00 +0200

    fix

--- src/commands/📚 Utility/support.js
+++ src/commands/📚 Utility/support.js
@@ -1,19 +1,19 @@
-const Discord = require('discord.js')
+const Discord = require("discord.js");
 
 module.exports = {
-  name: 'support',
-  category: '📚 Utility',
+  name: "support",
+  category: "📚 Utility",
   description: "Sends you Cody's bot support server link",
   cooldown: 3,
 
   run: async (client, message, args, user, guild) => {
     message.channel.send({
       embed: new Discord.MessageEmbed()
-        .setTitle('Support Server')
-        .setColor('GREEN')
+        .setTitle("Support Server")
+        .setColor("GREEN")
         .setDescription(
-          '[Click this to join our support server](https://discord.gg/npJg754WFp)'
-        )
-    })
-  }
-}
+          "[Click this to join our support server](https://discord.gg/nBF9GRcq8c)"
+        ),
+    });
+  },
+};
