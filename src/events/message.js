9fb1fde4790550e10d9e470d8091d26cbde27cca
Konrad570 <Konradtoesch@icloud.com>
2021-08-28 12:07:13 +0200

    message event

--- src/events/message.js
+++ src/events/message.js
@@ -52,6 +52,28 @@ client.on('message', async message => {
         )
     }
 
+    if (
+      !message.guild.me.hasPermission([
+        "EMBED_LINKS",
+        "SEND_MESSAGES",
+        "READ_MESSAGE_HISTORY",
+        "VIEW_CHANNEL"
+      ])
+    )
+      return message.channel.send(`
+      ❌ I require some Permissions!
+
+      **I need the following Permissions to work on your Server:**
+      EMBED_LINKS, 
+      SEND_MESSAGES, 
+      READ_MESSAGE_HISTORY,
+      VIEW_CHANNEL
+
+      ⚠️ Please add me the right Permissions and re-run this Command!
+  
+      `);
+
+
     if (command.cooldown) {
       if (Timeout.has(`${command.name}${message.author.id}`))
         return message.channel.send({
