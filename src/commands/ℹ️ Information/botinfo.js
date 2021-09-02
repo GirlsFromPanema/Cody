48ee83b89d6c6b4e743731681aa5f12d3f90d155
peterhanania <72696414+peterhanania@users.noreply.github.com>
2021-09-02 14:55:22 +0300

    fixed bot info

--- src/commands/ℹ️ Information/botinfo.js
+++ src/commands/ℹ️ Information/botinfo.js
@@ -1,15 +1,15 @@
-const Discord = require("discord.js");
-const User = require("../../database/schemas/User");
-const moment = require("moment");
-require("moment-duration-format");
-const { stripIndent } = require('common-tags');
+const Discord = require('discord.js')
+const User = require('../../database/schemas/User')
+const moment = require('moment')
+require('moment-duration-format')
+const { stripIndent } = require('common-tags')
 
 module.exports = {
-  name: "botinfo",
-  category: "ℹ️ Information",
+  name: 'botinfo',
+  category: 'ℹ️ Information',
   description: "Sends you Cody's information",
   cooldown: 5,
-  aliases: ["bi", "stats"],
+  aliases: ['bi', 'stats'],
 
   /**
    * @param {Client} client
@@ -18,14 +18,14 @@ module.exports = {
    */
 
   run: async (client, message, args, user, guild) => {
-    if(!message.guild.me.permissions.has("SEND_MESSAGES")) return;
+    if (!message.guild.me.permissions.has('SEND_MESSAGES')) return
     if (
       !message.guild.me.hasPermission([
-        "EMBED_LINKS",
-        "ADD_REACTIONS",
-        "SEND_MESSAGES",
-        "READ_MESSAGE_HISTORY",
-        "VIEW_CHANNEL",
+        'EMBED_LINKS',
+        'ADD_REACTIONS',
+        'SEND_MESSAGES',
+        'READ_MESSAGE_HISTORY',
+        'VIEW_CHANNEL'
       ])
     ) {
       return message.channel.send(`
@@ -40,48 +40,49 @@ module.exports = {
   
         ⚠️ Please add me the right Permissions and re-run this Command!
     
-        `);
+        `)
     }
 
+    const users = await User.find()
 
-    const users = await User.find(); 
-
-
-    let totalXP = 0;
-    if(users.length && users.length > 0){
-      for(let i = 0; i < users.length; i++){
-        const user = users[i];
-        if(user && user.xp){
+    let totalXP = 0
+    if (users.length && users.length > 0) {
+      for (let i = 0; i < users.length; i++) {
+        const user = users[i]
+        if (user && user.xp) {
           totalXP = totalXP + user.xp
         }
       }
     }
 
-
     const infos = stripIndent`
     Bot Information
     • Ping     : ${client.ws.ping}ms
-    • Uptime   : ${moment.duration(client.uptime).format("H [hours and] m [minutes]")}
+    • Uptime   : ${moment
+      .duration(client.uptime)
+      .format('H [hours and] m [minutes]')}
     • Servers  : ${client.guilds.cache.size}
     • Users    : ${client.users.cache.size}
     • Channels : ${client.channels.cache.size}\n
     Database Information: 
     - Users    : ${users.length} users
-    - Total XP : ${users.length > 1 ? users.reduce((a, b) => {return a.xp || 0 + b.xp || 0;}): users.xp || 0} XP`;
-    	
+    - Total XP : ${totalXP} xp`
+
     const embed = new Discord.MessageEmbed()
       .setTitle("Cody's Information")
-      .setColor("#2f3136")
+      .setColor('#2f3136')
       //.setColor(message.guild.me.displayHexColor)
       .setTimestamp()
-      .setFooter("cody-bot.xyz",  message.author.displayAvatarURL({ dynamic: true }))
+      .setFooter(
+        'cody-bot.xyz',
+        message.author.displayAvatarURL({ dynamic: true })
+      )
       .setDescription(`\`\`\`diff\n${infos}\`\`\``)
-        
 
-    return message.channel.send({ embed: embed });
-  },
-};
+    return message.channel.send({ embed: embed })
+  }
+}
 
-function UpperCase(inputString) {
-  return inputString.replace(inputString[0], inputString[0].toUpperCase());
+function UpperCase (inputString) {
+  return inputString.replace(inputString[0], inputString[0].toUpperCase())
 }
