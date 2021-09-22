2b40dc4b0bc9c7fff0eb2c446da4788636ef8640
Konrad570 <Konradtoesch@icloud.com>
2021-09-22 22:03:38 +0200

    Fixed ping

--- src/events/ping.js
+++ src/events/ping.js
@@ -8,7 +8,7 @@ const embed = new MessageEmbed()
 .setColor("BLURPLE")
 
 client.on("message", (message) => {
-    if(message.content.startsWith === `<@${client.user.id}>`) {
+    if(message.content.startsWith(`<@${client.user.id}>`)) {
         message.channel.send(embed)
     }
     //if(message.mentions.has(client.user)) 
