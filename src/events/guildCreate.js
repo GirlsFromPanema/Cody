73566ecf16caf19893cd702f0411a0de72be4b48
Konrad570 <Konradtoesch@icloud.com>
2021-10-12 21:10:49 +0200

    added guild add

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,32 +1,33 @@
-/*
 const client = require("../index");
 const { Discord, MessageEmbed, Guild } = require("discord.js");
 
+try {
 
 
-const embed = new MessageEmbed()                              
-  //.setAuthor(guild.name, guild.iconURL({ dynamic: true}))
+ client.on('guildCreate', async (guild, message) => {
+
+    const embed = new MessageEmbed()                              
   .setThumbnail("https://cdn.discordapp.com/attachments/876477209186017340/876820442562822184/Rd0c07cb2486f8c6aa515c4f9fc608357.png", {size: 1024})
   .setColor("BLURPLE")
   .setDescription([
-    '**Hey there, Cody here! Thanks for inviting me!** 👋',
+    '**Hey there, Cody here! Thanks for inviting me to your Server!** 👋',
     '',
     `Standard Prefix: **\`$\`**`,
     `Commands: **\`$help\`**`,
     '',
-    '    🌐**Help & Support**',
+    '    🌐 **Help & Support**',
     '• **[Status](https://cody-bot.xyz/status)**',
     '• **[Support Server](https://docs.Aeo.com)**',
     '• **[Contact us](https://cody-bot.xyz/contact)**',
     '',
     '',
-    '    💠**Other Links**',
+    '    💠 **Other Links**',
     '• **[Dashboard](https://cody-bot.xyz/panel)**',
     '• **[Invite Cody](https://cody-bot.xyz)**',
     '',
     '',
-    '    🔎**Information**',
-    '•  I am here to teach you Coding, I have many languages you can choose from',
+    '    🔎 **Information**',
+    '•  I am here to teach you Programming, I have many languages you can choose from',
     '•  From the Basic/Beginner Level to the advanced World of **JavaScript** - **Python** and more!',
     '•  You can configurate me trough my [Dashboard](https://cody-bot.xyz/panel)',
     '',
@@ -34,8 +35,8 @@ const embed = new MessageEmbed()
     '**Have a great day! 😎**',
     ]);
 
-
-client.on('guildCreate', (guild) => {
-    guild.systemChannel.send({ embed: embed }).catch(() => {});
+    guild.owner.send(embed).catch(() => {})
   });
-  */
+} catch(error) {
+    console.log(error)
+}
