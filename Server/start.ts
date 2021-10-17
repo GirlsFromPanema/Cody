cf5973fb2a69241db39679ac0ced7c18a366c289
Konrad570 <Konradtoesch@icloud.com>
2021-10-17 22:15:06 +0200

    removed Server, anti beta

--- Server/start.ts
+++
@@ -1,3 +0,0 @@
-// Cody deno Server | First go to server.ts
-// Open the Terminal and type: deno run --allow-net server.ts
-// Open Chrome with localhost:8000 | Make sure no other Program is running on that Host
