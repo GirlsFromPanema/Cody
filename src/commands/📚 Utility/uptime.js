7338c4fae9be6660a629490a58f84b32c49fa588
Konrad570 <Konradtoesch@icloud.com>
2021-10-20 17:51:05 +0200

    added new Handler + uptime

---
+++ src/commands/📚 Utility/uptime.js
@@ -0,0 +1,32 @@
+const { MessageEmbed } = require('discord.js')
+const { msToTimeObj } = require("../../handlers/util");
+
+
+module.exports = {
+    name : 'uptime',
+    category : 'info',
+    description : 'Returns uptime',
+
+    /**
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+
+    run : async(client, message, args) => {
+       
+        try {
+            const timeObj = msToTimeObj(message.client.uptime);
+
+            const embed = new MessageEmbed()
+            .setDescription(`**${timeObj.days}** days, **${timeObj.hours}** hours, **${timeObj.minutes}** minutes, **${timeObj.seconds}** seconds`)
+            .setFooter("cody-bot.xyz | <3")
+            message.channel.send(embed)
+
+        } catch(error) {
+            console.log(error)
+            message.channel.send("Error while executing Command! Please try again later. ")
+        }
+    }
+
+}
---
+++ src/handlers/util.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const path = require("path");
+
+
+/**
+ * Get the key for the given value in an object.
+ * @param {Object} object 
+ * @param {any} value 
+ * @returns Returns the key that corresponds to the value.
+ */
+module.exports.getKeyByValue = function (object, value)
+{
+    return Object.keys(object).find(key => object[key] === value);
+};
+
+/**
+ * Formats milliseconds to minutes and seconds. 298999 -> 4:59
+ * @param {Number} ms The amount of milliseconds to format.
+ * @returns Returns the formatted string.
+ */
+module.exports.msToMinAndSec = function (ms)
+{
+    var minutes = Math.floor(ms / 60000);
+    var seconds = ((ms % 60000) / 1000).toFixed(0);
+    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+};
+
+/**
+ * Formats the ms to days, hours, minutes and seconds and returns those values in an object.
+ * @param {Number} ms The amount of milliseconds to format.
+ */
+module.exports.msToTimeObj = function (ms)
+{
+    return {
+        days: Math.floor(ms / 86400000),
+        hours: Math.floor(ms / 3600000) % 24,
+        minutes: Math.floor(ms / 60000) % 60,
+        seconds: Math.floor(ms / 1000) % 60
+    };
+};
