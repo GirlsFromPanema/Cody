7054c6a2452816f4f1a84cf5664d7af6e6c3c86c
Konrad570 <Konradtoesch@icloud.com>
2021-12-10 15:32:11 +0100

    updated logs

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,26 +1,40 @@
-const client = require('../../src/index')
-const User = require('../database/schemas/User')
-const Guild = require('../database/schemas/Guild')
+const client = require("../../src/index");
+const User = require("../database/schemas/User");
+const Guild = require("../database/schemas/Guild");
+const fs = require("fs");
 
 // Logs when the Bot comes online, once.
-client.on('ready', async () => {
-  console.log(`${client.user.tag} is now online!`)
+client.on("ready", async () => {
+  console.log(`${client.user.tag} is now online!`);
+
+  const filename = `./Logs/logging.txt`;
+  const date = new Date().toLocaleString();
+  const content = `Time: ${date}\nSuccessfully logged in as ${client.user.tag}!`;
 
   // The Bots status
-  client.user.setActivity('$info • cody-bot.xyz', { type: 'PLAYING' })
+  client.user.setActivity("$info • cody-bot.xyz", { type: "PLAYING" });
 
   // Fetch the Users with the current Settings
-  const users = await User.find()
+  const users = await User.find();
   for (let user of users) {
-    client.userSettings.set(user.Id, user)
+    client.userSettings.set(user.Id, user);
   }
 
   // Fetch the Guild Settings for all Guilds Setup yet and load them into a loop.
-  const guilds = await Guild.find()
+  const guilds = await Guild.find();
   for (let guild of guilds) {
-    client.guildSettings.set(guild.Id, guild)
+    client.guildSettings.set(guild.Id, guild);
   }
 
+  // Creating logs for the Bot
+  fs.mkdir("Logs", (err) => {
+    if (err) return err;
+  });
+
+  fs.writeFile(filename, content, (err) => {
+    if (err) return err;
+  });
+
   // Check for any premium users on the current Client
-  require("../handlers/premium")(client)
-})
+  require("../handlers/premium")(client);
+});
