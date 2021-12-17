0861002e817becb9b949bf1de255a133c9259909
Konrad570 <Konradtoesch@icloud.com>
2021-12-17 15:48:33 +0100

    removed ping due to spam

--- src/events/ping.js
+++ src/events/ping.js
@@ -1,4 +1,4 @@
-const client = require("../index")
+/*const client = require("../index")
 const { MessageEmbed } = require("discord.js")
 
 const embed = new MessageEmbed()
@@ -16,3 +16,4 @@ client.on("message", (message) => {
         message.channel.send(embed)
     }
 })
+*/
